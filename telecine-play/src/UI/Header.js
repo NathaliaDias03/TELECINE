5502c8e586621f4efa6a4875f3a153054b637adf
msaatkamp <32959630+msaatkamp@users.noreply.github.com>
2019-04-09 00:49:12 -0300

    Header Telecine
    + Header com logo telecine

--- telecine-play/src/UI/Header.js
+++ telecine-play/src/UI/Header.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import './Header.scss';
-import logo from './../logo.svg';
+import logo from './../logo.png';
 
 const Header = () => {
     return (
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>
-              <button className="btn btn-primary btn-lg">
-                Erradu
-              </button>
-          </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-          </a>
+        <header className="Header">
+            <img src={logo} className="Header-Logo" alt="logo telecine play" />
         </header>
     );
 }
