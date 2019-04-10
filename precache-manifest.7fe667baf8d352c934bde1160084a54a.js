c35d8cc0ac7337870d46e31f225dbadee0593b13
msaatkamp <32959630+msaatkamp@users.noreply.github.com>
2019-04-10 03:11:37 -0300

    Updates

---
+++ precache-manifest.7fe667baf8d352c934bde1160084a54a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "535d3fc44de5fd9130b6",
+    "url": "/TELECINE/static/css/main.bddda852.chunk.css"
+  },
+  {
+    "revision": "535d3fc44de5fd9130b6",
+    "url": "/TELECINE/static/js/main.e1dc47a3.chunk.js"
+  },
+  {
+    "revision": "a7f48f8569954cd8053a",
+    "url": "/TELECINE/static/js/runtime~main.4d9d2f59.js"
+  },
+  {
+    "revision": "15932dc1c35b4bcf76c0",
+    "url": "/TELECINE/static/css/2.7bac985e.chunk.css"
+  },
+  {
+    "revision": "15932dc1c35b4bcf76c0",
+    "url": "/TELECINE/static/js/2.d987e58b.chunk.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/TELECINE/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/TELECINE/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/TELECINE/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/TELECINE/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "01d5410424fc12458451ec7ac0b854c1",
+    "url": "/TELECINE/index.html"
+  }
+];
