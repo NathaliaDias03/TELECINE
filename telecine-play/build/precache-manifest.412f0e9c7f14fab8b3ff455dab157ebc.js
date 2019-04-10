a97f99b6a2296572928704da8ce50da33f7c9b2b
msaatkamp <32959630+msaatkamp@users.noreply.github.com>
2019-04-10 02:02:14 -0300

    Express

---
+++ telecine-play/build/precache-manifest.412f0e9c7f14fab8b3ff455dab157ebc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6991e23431cbf2795ba8",
+    "url": "/static/css/main.bddda852.chunk.css"
+  },
+  {
+    "revision": "6991e23431cbf2795ba8",
+    "url": "/static/js/main.561fb289.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "490710a6ea83db9b5311",
+    "url": "/static/css/2.f6adc091.chunk.css"
+  },
+  {
+    "revision": "490710a6ea83db9b5311",
+    "url": "/static/js/2.d987e58b.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "3fa4e11bc8d07dabd7cad8d616a14e03",
+    "url": "/index.html"
+  }
+];
